b66eafec94de4f6c0c12290b8282a36b3c340899
Naoku <135733021+Naoku@users.noreply.github.com>
2024-05-19 19:26:46 +0200

    Add files via upload

--- GPU PASSTHROUGH/One GPU/revert.sh
+++ GPU PASSTHROUGH/One GPU/revert.sh
@@ -1,1 +1,27 @@
-a
+# unload vfio drivers
+modprobe -r vfio
+modprobe -r vfio_pci
+modprobe -r vfio_iommu_type1
+
+sleep 7 # stop race condition
+
+# make GPU free again
+virsh nodedev-reattach pci_0000_01_00_0 #GPU IDS CHANGE THIS 
+virsh nodedev-reattach pci_0000_01_00_1
+virsh nodedev-reattach pci_0000_01_00_2
+virsh nodedev-reattach pci_0000_01_00_3
+
+sleep 2 # stop race condition 
+
+# load nvidia drivers
+modprobe drm
+modprobe drm_kms_helper
+modprobe i2c_nvidia_gpu
+modprobe nvidia
+modprobe nvidia_modeset
+modprobe nvidia_drm
+modprobe nvidia_uvm
+
+sleep 3 # stop race condition
+
+systemctl start display-manager.service # start display-manager, KDE will load after logging in
